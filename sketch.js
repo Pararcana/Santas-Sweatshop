21c09a88ac5686e35aebd0174df048e1742ea200
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-12-29 10:45:50 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,6 @@ let presentArr = [];
 let ldm = false
 let mode = "timed"
 let menu = "game"
-let again
 
 let slowMo = 0
 let boost = 0
@@ -28,222 +27,86 @@ for (let i = 0; i < 1000; i++) {
 	veloArr.push(veloArr[veloArr.length - 1] + 1.25 * veloArr.length + Math.floor(i / 3) * 0.25)
 }
 
+function preload() {
+	snowflakes = {
+		20: loadImage("Trail/pixil-frame-0.png"),
+		19: loadImage("Trail/pixil-frame-0 (1).png"),
+		18: loadImage("Trail/pixil-frame-0 (2).png"),
+		17: loadImage("Trail/pixil-frame-0 (3).png"),
+		16: loadImage("Trail/pixil-frame-0 (4).png"),
+		15: loadImage("Trail/pixil-frame-0 (5).png"),
+		14: loadImage("Trail/pixil-frame-0 (6).png"),
+		13: loadImage("Trail/pixil-frame-0 (7).png"),
+		12: loadImage("Trail/pixil-frame-0 (8).png"),
+		11: loadImage("Trail/pixil-frame-0 (9).png"),
+		10: loadImage("Trail/pixil-frame-0 (10).png"),
+		9: loadImage("Trail/pixil-frame-0 (11).png"),
+		8: loadImage("Trail/pixil-frame-0 (12).png"),
+		7: loadImage("Trail/pixil-frame-0 (13).png"),
+		6: loadImage("Trail/pixil-frame-0 (14).png"),
+		5: loadImage("Trail/pixil-frame-0 (15).png"),
+		4: loadImage("Trail/pixil-frame-0 (16).png"),
+		3: loadImage("Trail/pixil-frame-0 (17).png"),
+		2: loadImage("Trail/pixil-frame-0 (18).png"),
+		1: loadImage("Trail/pixil-frame-0 (19).png")
+	}
+	presents = {
+		"red1": loadImage("Presents/pixil-frame-0.png"),
+		"red2": loadImage("Presents/pixil-frame-0 (1).png"),
+		"purple1": loadImage("Presents/pixil-frame-0 (2).png"),
+		"purple2": loadImage("Presents/pixil-frame-0 (3).png"),
+		"green1": loadImage("Presents/pixil-frame-0 (4).png"),
+		"green2": loadImage("Presents/pixil-frame-0 (5).png"),
+		"blue1": loadImage("Presents/pixil-frame-0 (6).png"),
+		"blue2": loadImage("Presents/pixil-frame-0 (7).png"),
+		"grey1": loadImage("Presents/pixil-frame-0 (8).png"),
+		"grey2": loadImage("Presents/pixil-frame-0 (9).png")
+	}
+	powerUps = {
+		"slowMo": loadImage("PowerUps/iceCream.png"),
+		"combo": loadImage("PowerUps/doughnut.png"),
+		"duration": loadImage("PowerUps/clock.png"),
+		"boost": loadImage("PowerUps/melon.png"),
+		"bomb": loadImage("PowerUps/bomb.png")
+	}
+	sfx = {
+		"rip": loadSound("SFX/rip.mp3"),
+		"rip1": loadSound("SFX/rip1.mp3"),
+		"rip2": loadSound("SFX/rip2.mp3"),
+		"ice": loadSound("SFX/ice.mp3"),
+		"break": loadSound("SFX/break.mp3"),
+		"rage": loadSound("SFX/rage.mp3"),
+		"time": loadSound("SFX/time.mp3"),
+		"boom": loadSound("SFX/boom.mp3"),
+		"squelch": loadSound("SFX/squelch.mp3")
+	}
+	songs = {
+		"xmas": loadSound("Songs/It's Christmas!.mp3")
+	}
+}
+
+function setup() {
+	randomPresent()
+	trailArr.push(new Snowflake())
+	background(100);
+	textFont("Courier New")
+	angleMode(DEGREES)
+	rectMode(CENTER)
+	imageMode(CENTER)
+	textAlign(CENTER)
+	resetTimed()
+	//songs["xmas"].loop()
+}
+
 function choice(arr) {
 	let index = Math.floor(Math.random() * arr.length);
 	return arr[index];
-}
-
-function resetTimed() {
-	mode = "timed"
-	menu = "game"
-	presentArr = []
-	timer = new Date().getTime() + 60000
-	charm = 0
-	comboCounter = 0
-	slowMo = 0
-	boost = 0
-	combo = 0
-	presentValue = 100
-	randomPresent(true)
 }
 
 function mouseBounds(xLB, xUB, yLB, yUB) {
 	let xBounds = windowWidth/2 + xLB <= mouseX && mouseX <= windowWidth/2 + xUB
 	let yBounds = windowHeight/2 + yLB <= mouseY && mouseY <= windowHeight/2 + yUB
 	return xBounds && yBounds
-}
-
-function handleTimer() {
-	push()
-	textSize(50)
-	strokeWeight(3)
-	stroke(((timer - currentTime)/1000 >= 6) && "grey" || "red")
-	text(Math.floor((timer - currentTime)/1000), windowWidth/2, 100)
-	pop()
-}
-
-function timedMode() {
-	if (currentTime <= timer) {
-		if (!Math.floor(Math.random() * 100)) {randomPresent(false)}
-		rect(windowWidth - 75, 35, 125, 50)
-		handleTimer()
-		handlePowerUps()
-		handlePresents()
-		comboHandler()
-		handleMouseText()
-	} else {
-		menu = "gOver"
-		handleGOverUI()
-	}
-}
-
-function startSlowMo() {
-	specialText = "=> Slow Motion: 50% Speed"
-	mouseText = "50% Speed"
-	mouseColor = [66, 135, 245]
-	sfx["ice"].play()
-	slowMo = currentTime + 5000
-}
-
-function startCombo() {
-	specialText = "=> Combo Doughnut: +5 Combo"
-	mouseText = "+5 Combo"
-	mouseColor = [245, 161, 66]
-	comboCounter += 5
-	sfx["rage"].play()
-	combo = currentTime + 5000
-}
-
-function addTime() {
-	specialText = "=> More Time: +3 seconds"
-	mouseText = "+3 Seconds"
-	mouseColor = [200, 200, 200]
-	sfx["time"].play()
-	timer += 3000
-}
-
-function explosion() {
-	specialText = "=> Bomb Exploded: -2500 Charm"
-	mouseText = "-2500 Charm"
-	mouseColor = [255, 0, 0]
-	sfx["boom"].play()
-	charm -= 2500
-}
-
-function startBoost() {
-	specialText = "=> Gained Melon: 2.5x Charm per Present"
-	mouseText = "2.5x Charm"
-	mouseColor = [133, 109, 16]
-	sfx["squelch"].play()
-	presentValue = 250
-	boost = currentTime + 5000
-}
-
-let yVelo = -50
-
-function randomPresent(cycle) {
-	let xPos = Math.floor(Math.random() * windowWidth)
-	let yPos = windowHeight + 25
-	let xVelo = Math.floor(Math.random() * 10) * (xPos < windowWidth/2 && -1 || 1)
-	let yVelo = -veloArr.indexOf(choice(veloArr.filter(v => v < windowHeight && v > windowHeight*0.6)))
-	let rot = Math.floor(Math.random() * 10) * choice([1, -1])
-	let skin = choice([...Object.keys(presents), ...Object.keys(powerUps)]) 
-	presentArr.push(new Present(xPos, yPos, xVelo, yVelo, rot, skin, cycle))
-}
-
-function trail(x, y, size) {
-	trailArr.push(new Snowflake())
-	for (let trail of trailArr) {
-		push()
-		trail.draw()
-		pop()
-	}
-	trailArr = trailArr.filter(v => v.state !== 0)
-}
-
-function comboHandler() {
-	if (currentTime <= comboUI[0]) {
-		let comboText = ""
-		let comboColor = []
-		if (comboUI[1] >= 50) {
-			comboText = ": IMPOSSIBLE COMBO!!!!!"
-			comboColor = [255, 0, 0]
-		} else if (comboUI[1] >= 25) {
-			comboText = ": LEGENDARY COMBO!!!"
-			comboColor = [168, 66, 50]
-		} else if (comboUI[1] >= 10) {
-			comboText = ": Insane Combo!"
-			comboColor = [255, 77, 0]
-		} else if (comboUI[1] >= 5) {
-			comboText = ": Nice Combo!"
-			comboColor = [245, 126, 7]
-		} else {
-			comboText = ": Combo!"
-			comboColor = [245, 173, 5]
-		}
-		if (comboUI[1] >= 3) {
-			mouseTimer = currentTime + 1000
-			mouseText = comboUI[1] + comboText
-			mouseColor = [...comboColor]
-		}
-	}
-}
-
-function comboCheck() {
-	if (currentTime <= combo) {
-		comboCounter++
-		combo = Math.max(currentTime + 500, combo)
-	} else {
-		if (comboCounter >= 3) {
-			sfx["break"].play()
-			charm += comboCounter * 100
-			specialText = `=> Combo: +${comboCounter*100} Charm`
-			specialTimer = currentTime + 1500
-		}
-		comboUI = [currentTime + 1500, comboCounter]
-		combo = currentTime + 500
-		comboCounter = 1
-	}
-}
-
-function handlePresents() {
-	for (let present of presentArr) {
-		push()
-		present.draw()
-		pop()
-	}
-}
-
-function drawPowerUpBar(x, y, col1, col2, t, name) {
-	push()
-	rectMode(CORNER)
-	fill(...col1)
-	square(x, y, 60, 10)
-	rect(x + 60, y + 15, 155, 30, 0, 5, 5, 0)
-	fill(...col2)
-	rect(x + 60, y + 20, 150 * Math.max(t-currentTime, 0)/5000, 20)
-	image(powerUps[name], x + 30, y + 30)
-	pop()
-}
-
-function handlePowerUps() {
-	if (boost <= currentTime) {presentValue = 100}
-	if (currentTime <= slowMo) {
-		push()
-		fill(3, 252, 248, 50)
-		rect(windowWidth/2, windowHeight/2, windowWidth, windowHeight)
-		pop()
-	}
-	drawPowerUpBar(25, 55, [3, 252, 248], [66, 135, 245], slowMo, "slowMo")
-	drawPowerUpBar(25, 125, [152, 219, 44], [17, 143, 44], boost, "boost")
-	drawPowerUpBar(25, 195, [217, 146, 85], [247, 116, 2], combo, "combo")
-
-}
-
-function handleMouseText() {
-	if (specialTimer <= currentTime) {specialText = ""}
-	push()
-	textAlign(LEFT)
-	stroke("pink")
-	text(`Charm: ${charm} ${specialText}`, 25, 35)
-	pop()
-	if (currentTime <= mouseTimer) {
-		push()
-		stroke(...mouseColor)
-		text(mouseText, mouseX, mouseY)
-		pop()
-	}
-}
-
-function handleGOverUI() {
-	push()
-	textSize(50)
-	strokeWeight(3)
-	stroke("pink")
-	text(`Charm: ${charm}`, windowWidth/2, windowHeight/2 - 75)
-	pop()
-	rect(windowWidth/2, windowHeight/2, 250, 50, 10)
-	rect(windowWidth/2, windowHeight/2 + 75, 250, 50, 10)
 }
 
 function slicingCollision(x, y) {
@@ -357,11 +220,219 @@ class Present {
 	}
 }
 
+function resetTimed() {
+	mode = "timed"
+	menu = "game"
+	presentArr = []
+	timer = new Date().getTime() + 60000
+	charm = 0
+	comboCounter = 0
+	slowMo = 0
+	boost = 0
+	combo = 0
+	presentValue = 100
+	randomPresent(true)
+}
+
+function handleTimer() {
+	push()
+	textSize(50)
+	strokeWeight(3)
+	stroke(((timer - currentTime)/1000 >= 6) && "grey" || "red")
+	text(Math.floor((timer - currentTime)/1000), windowWidth/2, 100)
+	pop()
+}
+
+function timedMode() {
+	if (currentTime <= timer) {
+		if (!Math.floor(Math.random() * 100)) {randomPresent(false)}
+		rect(windowWidth - 75, 35, 125, 50)
+		handleTimer()
+		handlePowerUps()
+		handlePresents()
+		comboHandler()
+		handleMouseText()
+	} else {
+		menu = "gOver"
+		handleGOverUI()
+	}
+}
+
+function addTime() {
+	specialText = "=> More Time: +3 seconds"
+	mouseText = "+3 Seconds"
+	mouseColor = [200, 200, 200]
+	sfx["time"].play()
+	timer += 3000
+}
+
+function comboHandler() {
+	if (currentTime <= comboUI[0]) {
+		let comboText = ""
+		let comboColor = []
+		if (comboUI[1] >= 50) {
+			comboText = ": IMPOSSIBLE COMBO!!!!!"
+			comboColor = [255, 0, 0]
+		} else if (comboUI[1] >= 25) {
+			comboText = ": LEGENDARY COMBO!!!"
+			comboColor = [168, 66, 50]
+		} else if (comboUI[1] >= 10) {
+			comboText = ": Insane Combo!"
+			comboColor = [255, 77, 0]
+		} else if (comboUI[1] >= 5) {
+			comboText = ": Nice Combo!"
+			comboColor = [245, 126, 7]
+		} else {
+			comboText = ": Combo!"
+			comboColor = [245, 173, 5]
+		}
+		if (comboUI[1] >= 3) {
+			mouseTimer = currentTime + 1000
+			mouseText = comboUI[1] + comboText
+			mouseColor = [...comboColor]
+		}
+	}
+}
+
+function comboCheck() {
+	if (currentTime <= combo) {
+		comboCounter++
+		combo = Math.max(currentTime + 500, combo)
+	} else {
+		if (comboCounter >= 3) {
+			sfx["break"].play()
+			charm += comboCounter * 100
+			specialText = `=> Combo: +${comboCounter*100} Charm`
+			specialTimer = currentTime + 1500
+		}
+		comboUI = [currentTime + 1500, comboCounter]
+		combo = currentTime + 500
+		comboCounter = 1
+	}
+}
+
+function startCombo() {
+	specialText = "=> Combo Doughnut: +5 Combo"
+	mouseText = "+5 Combo"
+	mouseColor = [245, 161, 66]
+	comboCounter += 5
+	sfx["rage"].play()
+	combo = currentTime + 5000
+}
+
+function startSlowMo() {
+	specialText = "=> Slow Motion: 50% Speed"
+	mouseText = "50% Speed"
+	mouseColor = [66, 135, 245]
+	sfx["ice"].play()
+	slowMo = currentTime + 5000
+}
+
+function explosion() {
+	specialText = "=> Bomb Exploded: -2500 Charm"
+	mouseText = "-2500 Charm"
+	mouseColor = [255, 0, 0]
+	sfx["boom"].play()
+	charm -= 2500
+}
+
+function startBoost() {
+	specialText = "=> Gained Melon: 2.5x Charm per Present"
+	mouseText = "2.5x Charm"
+	mouseColor = [133, 109, 16]
+	sfx["squelch"].play()
+	presentValue = 250
+	boost = currentTime + 5000
+}
+
+function randomPresent(cycle) {
+	let xPos = Math.floor(Math.random() * windowWidth)
+	let yPos = windowHeight + 25
+	let xVelo = Math.floor(Math.random() * 10) * (xPos < windowWidth/2 && -1 || 1)
+	let yVelo = -veloArr.indexOf(choice(veloArr.filter(v => v < windowHeight && v > windowHeight*0.6)))
+	let rot = Math.floor(Math.random() * 10) * choice([1, -1])
+	let skin = choice([...Object.keys(presents), ...Object.keys(powerUps)]) 
+	presentArr.push(new Present(xPos, yPos, xVelo, yVelo, rot, skin, cycle))
+}
+
+function handlePresents() {
+	for (let present of presentArr) {
+		push()
+		present.draw()
+		pop()
+	}
+}
+
+function trail(x, y, size) {
+	trailArr.push(new Snowflake())
+	for (let trail of trailArr) {
+		push()
+		trail.draw()
+		pop()
+	}
+	trailArr = trailArr.filter(v => v.state !== 0)
+}
+
+function drawPowerUpBar(x, y, col1, col2, t, name) {
+	push()
+	rectMode(CORNER)
+	fill(...col1)
+	square(x, y, 60, 10)
+	rect(x + 60, y + 15, 155, 30, 0, 5, 5, 0)
+	fill(...col2)
+	rect(x + 60, y + 20, 150 * Math.max(t-currentTime, 0)/5000, 20)
+	image(powerUps[name], x + 30, y + 30)
+	pop()
+}
+
+function handlePowerUps() {
+	if (boost <= currentTime) {presentValue = 100}
+	if (currentTime <= slowMo) {
+		push()
+		fill(3, 252, 248, 50)
+		rect(windowWidth/2, windowHeight/2, windowWidth, windowHeight)
+		pop()
+	}
+	drawPowerUpBar(25, 55, [3, 252, 248], [66, 135, 245], slowMo, "slowMo")
+	drawPowerUpBar(25, 125, [152, 219, 44], [17, 143, 44], boost, "boost")
+	drawPowerUpBar(25, 195, [217, 146, 85], [247, 116, 2], combo, "combo")
+
+}
+
+function handleMouseText() {
+	if (specialTimer <= currentTime) {specialText = ""}
+	push()
+	textAlign(LEFT)
+	stroke("pink")
+	text(`Charm: ${charm} ${specialText}`, 25, 35)
+	pop()
+	if (currentTime <= mouseTimer) {
+		push()
+		stroke(...mouseColor)
+		text(mouseText, mouseX, mouseY)
+		pop()
+	}
+}
+
+function handleGOverUI() {
+	push()
+	textSize(50)
+	strokeWeight(3)
+	stroke("pink")
+	text(`Charm: ${charm}`, windowWidth/2, windowHeight/2 - 75)
+	pop()
+	rect(windowWidth/2, windowHeight/2, 250, 50, 10)
+	rect(windowWidth/2, windowHeight/2 + 75, 250, 50, 10)
+}
+
 function keyPressed() {if (key == "l") {ldm = !ldm}}
 
 function mousePressed() {
 	switch (menu) {
-		case "game": break;
+		case "game":
+			//rect(windowWidth - 75, 35, 125, 50)
+
+			break;
 		case "gOver":
 			if (mouseBounds(-125, 125, -25, 25)) {
 				switch (mode) {
@@ -371,77 +442,6 @@ function mousePressed() {
 			else if (mouseBounds(-125, 125, 75, 100)) {menu = "main"; mode = "main"}
 			break;
 	}
-}
-
-function preload() {
-	snowflakes = {
-		20: loadImage("Trail/pixil-frame-0.png"),
-		19: loadImage("Trail/pixil-frame-0 (1).png"),
-		18: loadImage("Trail/pixil-frame-0 (2).png"),
-		17: loadImage("Trail/pixil-frame-0 (3).png"),
-		16: loadImage("Trail/pixil-frame-0 (4).png"),
-		15: loadImage("Trail/pixil-frame-0 (5).png"),
-		14: loadImage("Trail/pixil-frame-0 (6).png"),
-		13: loadImage("Trail/pixil-frame-0 (7).png"),
-		12: loadImage("Trail/pixil-frame-0 (8).png"),
-		11: loadImage("Trail/pixil-frame-0 (9).png"),
-		10: loadImage("Trail/pixil-frame-0 (10).png"),
-		9: loadImage("Trail/pixil-frame-0 (11).png"),
-		8: loadImage("Trail/pixil-frame-0 (12).png"),
-		7: loadImage("Trail/pixil-frame-0 (13).png"),
-		6: loadImage("Trail/pixil-frame-0 (14).png"),
-		5: loadImage("Trail/pixil-frame-0 (15).png"),
-		4: loadImage("Trail/pixil-frame-0 (16).png"),
-		3: loadImage("Trail/pixil-frame-0 (17).png"),
-		2: loadImage("Trail/pixil-frame-0 (18).png"),
-		1: loadImage("Trail/pixil-frame-0 (19).png")
-	}
-	presents = {
-		"red1": loadImage("Presents/pixil-frame-0.png"),
-		"red2": loadImage("Presents/pixil-frame-0 (1).png"),
-		"purple1": loadImage("Presents/pixil-frame-0 (2).png"),
-		"purple2": loadImage("Presents/pixil-frame-0 (3).png"),
-		"green1": loadImage("Presents/pixil-frame-0 (4).png"),
-		"green2": loadImage("Presents/pixil-frame-0 (5).png"),
-		"blue1": loadImage("Presents/pixil-frame-0 (6).png"),
-		"blue2": loadImage("Presents/pixil-frame-0 (7).png"),
-		"grey1": loadImage("Presents/pixil-frame-0 (8).png"),
-		"grey2": loadImage("Presents/pixil-frame-0 (9).png")
-	}
-	powerUps = {
-		"slowMo": loadImage("PowerUps/iceCream.png"),
-		"combo": loadImage("PowerUps/doughnut.png"),
-		"duration": loadImage("PowerUps/clock.png"),
-		"boost": loadImage("PowerUps/melon.png"),
-		"bomb": loadImage("PowerUps/bomb.png")
-	}
-	sfx = {
-		"rip": loadSound("SFX/rip.mp3"),
-		"rip1": loadSound("SFX/rip1.mp3"),
-		"rip2": loadSound("SFX/rip2.mp3"),
-		"ice": loadSound("SFX/ice.mp3"),
-		"break": loadSound("SFX/break.mp3"),
-		"rage": loadSound("SFX/rage.mp3"),
-		"time": loadSound("SFX/time.mp3"),
-		"boom": loadSound("SFX/boom.mp3"),
-		"squelch": loadSound("SFX/squelch.mp3")
-	}
-	songs = {
-		"xmas": loadSound("Songs/It's Christmas!.mp3")
-	}
-}
-
-function setup() {
-	randomPresent()
-	trailArr.push(new Snowflake())
-	background(100);
-	textFont("Courier New")
-	angleMode(DEGREES)
-	rectMode(CENTER)
-	imageMode(CENTER)
-	textAlign(CENTER)
-	resetTimed()
-	//songs["xmas"].loop()
 }
 
 function draw() {
